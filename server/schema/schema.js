33539bf8326bd76b25ac5d0b03f6868c9b8ae159
Abhishek Sharma <abhi243461@hotmail.com>
2020-09-17 21:10:42 +0530

    feat: GraphQL schema and root query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,43 @@
 const graphql = require('graphql')
+const Word = require('../models/Word')
 
-const { GraphQLObjectType, GraphQLString } = graphql
+const {
+    GraphQLObjectType,
+    GraphQLList,
+    GraphQLString,
+    GraphQLID,
+} = graphql
 
 const WordType = new GraphQLObjectType({
     name: 'Word',
     fields: () => ({
-        id: { type: GraphQLString },
-        name: { type: GraphQLString }
+        id: { type: GraphQLID },
+        word: { type: GraphQLString },
+        pronounciationUrl: { type: GraphQLString },
+        origin: new GraphQLList(GraphQLString),
+        entries: new GraphQLList(new GraphQLObjectType({
+            partOfSpeech: { type: GraphQLString },
+            definition: { type: GraphQLString },
+            examples: new GraphQLList(GraphQLString)
+        }))
     })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        word: {
+            type: WordType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return Word.findById(args.id)
+            }
+        },
+        words: {
+            type: new GraphQLList(WordType),
+            resolve(parent, args) {
+                return Word.find()
+            }
+        }
+    }
 })
